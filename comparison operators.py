bc6e59bfdb5c6ab0d2ff067220c834751de870d0
manohar15 <63805651+manohar15@users.noreply.github.com>
2020-10-02 21:39:24 +0530

    code of comparison operators
    this is the code for comparison operators

---
+++ comparison operators.py
@@ -0,0 +1,10 @@
+x = 5 
+if x == 5:
+  print('Equal to 5')
+if x > 4:
+    print('Greater than 4')
+if x >= 5:
+   print('Greater than or equal to 5')
+if x <6 : print('Less than 6')
+if x != 6 : 
+   print('Not equal to 6')
