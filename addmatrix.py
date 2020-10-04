99291cd1695e43ea99e23e6bd87936b97ec16598
AnandaKrishnanNair <65528160+AnandaKrishnanNair@users.noreply.github.com>
2020-10-04 20:06:47 +0530

    added matrix addition

---
+++ addmatrix.py
@@ -0,0 +1,24 @@
+#https://m.facebook.com/story.php?story_fbid=348161766403412&id=100036287730638&ref=bookmarks
+#subscribed by Ananda Krishnan Nair
+# Program to add two matrices using nested loop
+
+X = [[12,7,3],
+    [4 ,5,6],
+    [7 ,8,9]]
+
+Y = [[5,8,1],
+    [6,7,3],
+    [4,5,9]]
+
+result = [[0,0,0],
+         [0,0,0],
+         [0,0,0]]
+
+# iterate through rows
+for i in range(len(X)):
+   # iterate through columns
+   for j in range(len(X[0])):
+       result[i][j] = X[i][j] + Y[i][j]
+
+for r in result:
+   print(r)
