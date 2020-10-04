56dce93968abe9894aef4bc629c69d08cc2feb14
AnandaKrishnanNair <65528160+AnandaKrishnanNair@users.noreply.github.com>
2020-10-04 19:46:12 +0530

    added Star Pyramid Program

---
+++ starpyramidcode.py
@@ -0,0 +1,8 @@
+#https://m.facebook.com/story.php?story_fbid=348161766403412&id=100036287730638&ref=bookmarks
+#subscribed by Ananda Krishnan Nair
+a=int(input("Enter the Number :"))
+b=a+1
+for i in range(b):
+    for j in range(i):
+        print("*",end="")
+    print()
