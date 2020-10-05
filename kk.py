9da7c02671fc492cf052ed3af8e2be2ca744eaa9
kk3600 <67818713+kk3600@users.noreply.github.com>
2020-10-05 19:40:30 +0530

    added new file of armstrong number

---
+++ kk.py
@@ -0,0 +1,19 @@
+# https://www.facebook.com/swati.mallik.180/posts/158104889312424
+# subscribed by swati
+num = int(input("Enter a number: "))
+
+# initialize sum
+sum = 0
+
+# find the sum of the cube of each digit
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** 3
+   temp //= 10
+
+# display the result
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
